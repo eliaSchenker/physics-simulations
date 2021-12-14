609883ae67a3e34f5e8bbc684ebe0436fa60c008
EliaSchenker <schenkere@bzz.ch>
2021-12-14 18:02:32 +0100

    Create pageloader.js
    Added pageloader for loading Navbar

---
+++ pageloader.js
@@ -0,0 +1,16 @@
+function initPage() {
+    document.getElementsByTagName("nav")[0].innerHTML = `
+        <div class="navbar">
+        <a href="https://eliaschenker.com/physics-simulations">Home</a>
+        <div class="dropdown">
+            <button class="dropbtn">Newtons's law of universal gravitation 
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+                <a href="https://eliaschenker.com/physics-simulations/newtonian_force/MoonAndEarth.html">Moon and earth (Centripetal force)</a>
+                <a href="https://eliaschenker.com/physics-simulations/newtonian_force/solarsystem.html">Solar system</a>
+                <a href="https://eliaschenker.com/physics-simulations/newtonian_force/trinarysystem.html">Trinary star system</a>
+            </div>
+        </div>
+     `;
+}
