6c321d8dd21b882c4a8101ad4ddc35e35a062077
EliaSchenker <schenkere@bzz.ch>
2021-12-18 09:17:17 +0100

    Create ObjectUtil.js
    Added new ObjectUtil class for object operations

---
+++ util/ObjectUtil.js
@@ -0,0 +1,20 @@
+class ObjectUtil {
+    static clone(obj) {
+        if (obj === null || typeof (obj) !== 'object' || 'isActiveClone' in obj)
+            return obj;
+    
+        if (obj instanceof Date)
+            var temp = new obj.constructor(); //or new Date(obj);
+        else
+            var temp = new obj.constructor();
+    
+        for (var key in obj) {
+            if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                obj['isActiveClone'] = null;
+                temp[key] = ObjectUtil.clone(obj[key]);
+                delete obj['isActiveClone'];
+            }
+        }
+        return temp;
+    }
+}
