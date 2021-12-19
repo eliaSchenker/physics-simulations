baf3009947f3a669ed0966883a4b7bef55f73cca
EliaSchenker <schenkere@bzz.ch>
2021-12-19 19:55:44 +0100

    Update GravitationalFieldVisualizer.js
    -Made the Gravitational Field rendering more accurate and fast
    -Removed the .at function in getFieldPoints because it isn't support on Safari

--- newtonian_force/GravitationalFieldVisualizer.js
+++ newtonian_force/GravitationalFieldVisualizer.js
@@ -5,7 +5,6 @@ class GravitationalFieldVisualizer {
         this.physicalBodies = [];
         this.testBody = new PhysicalBody(1, 1000000, new Vector2(0, 0), new Vector2(0, 0), "");
         this.lineAmount = 50;
-        this.arrowDistance = 7000000;
         this.arrowStartDistance = 100000000;
     }
 
@@ -37,6 +36,8 @@ class GravitationalFieldVisualizer {
     }
 
     getFieldPoints(bodies, pointAmount, startDistance, arrowDistance) {
+
+
         let result = []; //Result array consisting of arrays of points
         let initialPoints = [];
         let averagePosX = 0;
@@ -80,12 +81,22 @@ class GravitationalFieldVisualizer {
                     let radianAngle = this.getAccelerationAtPoint(initialPoints[j]).getAngleRadians();
                     let newPoint = initialPoints[j].moveAtAngle(radianAngle, arrowDistance);
                     initialPoints[j] = newPoint;
+                    
+                    let inRadiusBodyIndex = -1;
                     for (let k = 0; k < bodies.length; k++) {
                         if(newPoint.distanceTo(bodies[k].position) < bodies[k].radius) {
+                            inRadiusBodyIndex = k;
                             initialPoints[j] = undefined;
                         }
                     }
-                    result[j].push(newPoint);
+                    if(inRadiusBodyIndex == -1) {
+                        result[j].push(newPoint);
+                    }else {
+                        let distance = result[j][result[j].length - 1].distanceTo(bodies[inRadiusBodyIndex].position);
+                        let distanceToBodyBorder = distance - bodies[inRadiusBodyIndex].radius;
+                        let angleToBody = result[j][result[j].length - 1].angleTo(bodies[inRadiusBodyIndex].position);
+                        result[j].push(result[j][result[j].length - 1].moveAtAngle(angleToBody, distanceToBodyBorder));
+                    }
                 }
             }
         }
@@ -104,10 +115,24 @@ class GravitationalFieldVisualizer {
             body.addInteractionEvents(undefined, (function(e) {this.physicalBodies[tempIndex].position = e}).bind(this));
             this.renderer.toRenderObjects.push(body);
         }
-        let fieldPoints = this.getFieldPoints(this.physicalBodies, this.lineAmount, this.arrowStartDistance, this.arrowDistance);
+        
+        let fieldPoints = this.getFieldPoints(this.physicalBodies, this.lineAmount, this.arrowStartDistance, 1000000);
+        
         for (let i = 0; i < fieldPoints.length; i++) {
             for (let j = 0; j < fieldPoints[i].length - 1; j++) {
-                this.renderer.toRenderObjects.push(new ArrowRenderObject(fieldPoints[i][j], fieldPoints[i][j + 1]));
+                if(j % 20 == 0 && j != 0) {
+                    let angle = fieldPoints[i][j].angleTo(fieldPoints[i][j - 1]);
+                    let leftArrowPoint = fieldPoints[i][j].moveAtAngle(angle + 0.785398, 1000000);
+                    let rightArrowPoint = fieldPoints[i][j].moveAtAngle(angle - 0.785398, 1000000);
+
+                    this.renderer.toRenderObjects.push(new LineRenderObject(fieldPoints[i][j], leftArrowPoint));
+                    this.renderer.toRenderObjects.push(new LineRenderObject(fieldPoints[i][j], rightArrowPoint));
+                }
+                if(j != fieldPoints[i].length - 2) {
+                    this.renderer.toRenderObjects.push(new LineRenderObject(fieldPoints[i][j], fieldPoints[i][j + 1], 1, "#000000"));
+                }else {
+                    this.renderer.toRenderObjects.push(new LineRenderObject(fieldPoints[i][j], fieldPoints[i][j + 1], 1, "#000000"));
+                }
             }
         }
 
